526f8ba40d6564a5124ee6fcd4ce56ddfe27eeb6
Hadeed711 <hadeedahmad711@gmail.com>
2026-06-22 16:49:28 +0500

    ddd

--- shortener.py
+++ shortener.py
@@ -1,5 +1,6 @@
 import hashlib
 
+
 def generate_code(long_url: str) -> str:
     """Return a 6-char alphanumeric code for a given URL."""
     hash_bytes = hashlib.md5(long_url.encode()).hexdigest()
